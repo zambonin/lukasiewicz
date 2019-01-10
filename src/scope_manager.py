0ab510bdd6e234add9833c774cff7df5b8796d03
Gustavo Zambonin <gzmbnn@gmail.com>
2019-01-10 15:16:20 -0200

    pylint fixes on scope_manager

--- src/scope_manager.py
+++ src/scope_manager.py
@@ -1,21 +1,22 @@
-from sys import modules
-
 """scope_manager.py
 
 Defines two helper functions that mimic Łukasiewicz
 behaviour regarding lexical scopes. Based on [1].
 
-[1] http://stackoverflow.com/a/21795428
-"""
+[1] http://stackoverflow.com/a/21795428"""
+
+from sys import modules
 
 __context__ = {}
 
 
 def s_context():
+    """Saves the state of a namespace."""
     __context__.update(modules[__name__].__dict__)
 
 
 def r_context():
+    """Restores the state of a namespace."""
     for _ in list(modules[__name__].__dict__.keys()):
         if _ not in __context__:
             del modules[__name__].__dict__[_]
